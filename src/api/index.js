33629707cd41dabdbdd8abfcb361a0f986368480
Ben Garren <bngarren@gmail.com>
2022-06-15 20:21:29 -0400

    WIP

--- src/api/index.js
+++ src/api/index.js
@@ -19,21 +19,6 @@ app.use(
     allowedHeaders: ["Content-Type"],
   })
 );
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, Origin"
-  );
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-  next();
-});
 app.options("*", cors());
 
 const db = require("./db/db.js");
